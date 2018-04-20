6cb3fea650f644ce7aef13df61cd6f3a21030c23
Erica Suh <ekjsuh@gmail.com>
2018-04-20 16:41:09 -0600

    small fixes

---
+++ submissions/submission.py
@@ -0,0 +1,49 @@
+import sys
+
+# command line: python submission.py input.tsv modelNo taxonID goID "keywords"
+
+# Filename Format
+def create_outFile_name(model, taxon, go):
+    outFile_name = "BYUBRG18_" + model + "_" + taxon + "_" + go + ".txt"
+    return outFile_name
+
+# File Header
+def create_header(model, keyword):
+    header = "AUTHOR\tBYUBRG18\nMODEL\t" + model + "\nKEYWORDS\t" + keyword + "\n"
+    return header
+
+# Create dictionary
+def create_dictionary(file):
+    predictions_dict = {}
+    for line in file:
+        if line[0] != "C":
+            line = line.strip()
+            column = line.split("\t")
+            cafa = column[0]
+            prob = str(format(float(column[1]), '.2f'))
+            predictions_dict[cafa] = prob
+    return predictions_dict
+
+# Predictions Format {cafa-id: probability}
+def format_predictions(my_dict):
+    prediction = ""
+    for cafa in my_dict:
+        prediction += cafa + "\t" + my_dict[cafa] + "\n"
+    return prediction
+
+
+if __name__ == "__main__":
+    model_no = sys.argv[2]
+    taxon_id = sys.argv[3]
+    go_id = sys.argv[4]
+    key_words = sys.argv[5]
+
+    prediction_file = open(sys.argv[1], 'r')
+    predictions = create_dictionary(prediction_file)
+    prediction_file.close()
+
+    outFile = open(create_outFile_name(model_no, taxon_id, go_id),'w')
+    outFile.write(create_header(model_no, key_words))
+    outFile.write(format_predictions(predictions))
+    outFile.write("END")
+    outFile.close()
